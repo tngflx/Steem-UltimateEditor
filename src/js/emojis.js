e2ddcedb5962a602d0208b8b36e50737d835f645
Felix Tang <36429783+tngflx@users.noreply.github.com>
2018-06-02 19:20:29 +0800

    Emoji feature

---
+++ src/js/emojis.js
@@ -0,0 +1,84 @@
+function extend(dest, source) {
+        var prop;
+        dest = dest || {};
+        for (prop in source) {
+            if (source.hasOwnProperty(prop) && !dest.hasOwnProperty(prop)) {
+                dest[prop] = source[prop];
+            }
+        }
+        return dest;
+    }
+
+    function getSelectionText(doc) {
+        if (doc.getSelection) {
+            return doc.getSelection().toString();
+        }
+        if (doc.selection && doc.selection.type !== 'Control') {
+            return doc.selection.createRange().text;
+        }
+        return '';
+    }
+
+    function getSelectionStart(doc) {
+        var node = doc.getSelection().anchorNode,
+            startNode = (node && node.nodeType === 3 ? node.parentNode : node);
+
+        return startNode;
+    }
+
+    function placeCaretAtNode(doc, node, before) {
+        if (doc.getSelection !== undefined && node) {
+            var range = doc.createRange(),
+                selection = doc.getSelection();
+
+            if (before) {
+                range.setStartBefore(node);
+            } else {
+                range.setStartAfter(node);
+            }
+
+            range.collapse(false);
+
+            selection.removeAllRanges();
+            selection.addRange(range);
+        }
+    }
+
+    function isInsideElementOfTag(node, tag) {
+        if (!node) {
+            return false;
+        }
+
+        var parentNode = node.parentNode,
+            tagName = parentNode.tagName.toLowerCase();
+
+        while (tagName !== 'body') {
+            if (tagName === tag) {
+                return true;
+            }
+            parentNode = parentNode.parentNode;
+
+            if (parentNode && parentNode.tagName) {
+                tagName = parentNode.tagName.toLowerCase();
+            } else {
+                return false;
+            }
+        }
+
+        return false;
+    }
+
+    function getParentOf(el, tagTarget) {
+        var tagName = el && el.tagName ? el.tagName.toLowerCase() : false;
+
+        if (!tagName) {
+            return false;
+        }
+        while (tagName && tagName !== 'body') {
+            if (tagName === tagTarget) {
+                return el;
+            }
+            el = el.parentNode;
+            tagName = el && el.tagName ? el.tagName.toLowerCase() : false;
+        }
+    }
